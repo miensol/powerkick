519cc1dc9d212f48e2072b00f0e12fb646f99e04
Piotr Mionskowski <piotr.mionskowski@gmail.com>
2012-06-30 21:35:51 +0200

    added missing log tests

---
+++ specs/powerkick-log.Tests.ps1
@@ -0,0 +1,81 @@
+$src = (split-path -parent (Split-Path -Parent $MyInvocation.MyCommand.Path)) + "\src\powerkick"
+
+function ReloadModule {
+	Import-Module "$src\powerkick-log.psm1" -Force | Out-Null		
+}
+
+Describe "getting log file" {	
+	
+	It "by default it should return file in current directory" {				
+		ReloadModule 
+		(Get-LogFile).should.be("$(pwd)\log.txt")
+	}			
+	
+	It "should allow setting file name externaly" {
+		ReloadModule 
+		(Set-LogFileName "newlogname")
+		(Get-LogFile).should.be("$(pwd)\newlogname")
+	}
+	
+	it "should properly set file name from current time and machie name" {
+		ReloadModule 
+		$dateTime = [System.DateTime]::Now;
+		Set-LogFileNameFromCurrentTime "$(pwd)\logs"
+		$logName = (Get-LogFile)
+		$direcotry  = (Split-Path -Parent $logName)
+		$fileName = (Split-Path -Leaf $logName)
+		$direcotry.should.be("$(pwd)\logs")
+		$fileName.should.match("powerkick-log")
+		$fileName.should.match($dateTime.Year)
+		$fileName.should.match($dateTime.Month)
+		$fileName.should.match($dateTime.Day)
+		$fileName.should.match($dateTime.Hour)
+		$fileName.should.match($dateTime.Minute)
+		$fileName.should.match($Env:COMPUTERNAME)
+	}
+}
+
+Describe "getting transcript file" {
+	
+	It "by default should return file in current directory" {
+		ReloadModule 
+		(Get-TranscriptLogFile).should.be("$(pwd)\log-transcript.txt")
+	}
+	
+	It "should properly set file name from current date time " {
+		ReloadModule 
+		$dateTime = [System.DateTime]::Now;
+		Set-LogFileNameFromCurrentTime "$(pwd)\logs"
+		$logName = (Get-TranscriptLogFile)
+		$direcotry  = (Split-Path -Parent $logName)
+		$fileName = (Split-Path -Leaf $logName)
+		$logName.should.match("powerkick-log-transcript")
+		
+	}
+}
+
+
+Describe "logging" {
+	
+	Setup -File "log.txt" ""
+	In $TestDrive {
+		ReloadModule 
+	}
+	
+	It "should properly log info" {
+		(Get-Log).Info("information")
+		(Get-Content "$TestDrive\log.txt").should.match("information")
+	}
+	
+	It "should properly log warning" {
+		(Get-Log).Warning("warningmessage")
+		(Get-Content "$TestDrive\log.txt").should.match("warningmessage")
+	}
+	
+	It "should properly log error" {
+		(Get-Log).Error("errormessage")
+		(Get-Content "$TestDrive\log.txt").should.match("errormessage")
+	}		
+
+}
+
